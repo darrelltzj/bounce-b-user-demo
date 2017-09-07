ff79a02f242b2dee2b935ae5175ccad8eeb7e75d
dorkblue <d.dorkblue@gmail.com>
2017-09-07 16:32:21 +0800

    added recommended to info

--- src/containers/Info.js
+++ src/containers/Info.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Row, Col, Card } from 'antd'
 
+import Recommended from '../components/partials/Recommended'
 import { shortDescription, title } from '../constants/placeholders'
 
 const StyledHomePage = styled.div`
@@ -45,58 +46,65 @@ export default class Info extends React.Component {
   render () {
     return (
       <StyledHomePage>
-        <header>
-          <span>Ginseng</span>
-        </header>
-        <main>
-          <section>
-            <Row>
-              <Col xs={24} sm={18} md={18} style={{paddingRight: '1%'}}>
-                <span className='info-subheader'>
-                  人蔘 | 人参 | 인삼
-              </span>
-                <p>
-                  {shortDescription}
-                </p>
-              </Col>
-            </Row>
-          </section>
-          <section>
-            <Row>
-              <Col xs={24} sm={18} md={18} style={{paddingRight: '1%'}}>
-                <span className='info-subheader'>
-                  Types
-              </span>
-                <div style={{ background: '#ECECEC', padding: '30px' }}>
-                  <Row gutter={16}>
-                    <Col span={8}>
-                      <Card title='American' bordered={false}>{shortDescription}</Card>
-                    </Col>
-                    <Col span={8}>
-                      <Card title='Asian' bordered={false}>{shortDescription}</Card>
-                    </Col>
-                    <Col span={8}>
-                      <Card title='Wild' bordered={false}>{shortDescription}</Card>
-                    </Col>
-                  </Row>
-                </div>
-              </Col>
-            </Row>
-          </section>
-          <section>
-            <Col xs={24} sm={18} md={18} style={{paddingRight: '1%'}}>
-              <span className='info-subheader'>
-                Benefits
-              </span>
-              <ul>
-                <li>1. {title}</li>
-                <li>2. {title}</li>
-                <li>3. {title}</li>
-                <li>4. {title}</li>
-              </ul>
-            </Col>
-          </section>
-        </main>
+        <Row>
+          <Col xs={24} sm={18} md={18} style={{paddingRight: '1%'}}>
+            <header>
+              <span>Ginseng</span>
+            </header>
+            <main>
+              <section>
+                <Row>
+                  <Col xs={24} sm={24} md={24} style={{paddingRight: '1%'}}>
+                    <span className='info-subheader'>
+                      人蔘 | 人参 | 인삼
+                  </span>
+                    <p>
+                      {shortDescription}
+                    </p>
+                  </Col>
+                </Row>
+              </section>
+              <section>
+                <Row>
+                  <Col xs={24} sm={24} md={24} style={{paddingRight: '1%'}}>
+                    <span className='info-subheader'>
+                      Types
+                  </span>
+                    <div style={{ background: '#ECECEC', padding: '30px' }}>
+                      <Row gutter={16}>
+                        <Col span={8}>
+                          <Card title='American' bordered={false}>{shortDescription}</Card>
+                        </Col>
+                        <Col span={8}>
+                          <Card title='Asian' bordered={false}>{shortDescription}</Card>
+                        </Col>
+                        <Col span={8}>
+                          <Card title='Wild' bordered={false}>{shortDescription}</Card>
+                        </Col>
+                      </Row>
+                    </div>
+                  </Col>
+                </Row>
+              </section>
+              <section>
+                <Col xs={24} sm={24} md={24} style={{paddingRight: '1%'}}>
+                  <span className='info-subheader'>
+                    Benefits
+                  </span>
+                  <ul>
+                    <li>1. {title}</li>
+                    <li>2. {title}</li>
+                    <li>3. {title}</li>
+                    <li>4. {title}</li>
+                  </ul>
+                </Col>
+              </section>
+            </main>
+          </Col>
+          <Col xs={24} sm={6} md={6} style={{paddingLeft: '1%'}}>
+            <Recommended />
+          </Col>
+        </Row>
       </StyledHomePage>
 
     )
